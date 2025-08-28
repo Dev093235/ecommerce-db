2b67cd5e3ab276498bc9f4f0cc9b7b8cecc7a782
Dev093235 <dev431140@gmail.com>
2025-08-28 10:04:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
 const productRoutes = require('./routes/products');
 
 const app = express();
@@ -12,7 +10,4 @@ app.use(express.json());
 app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
