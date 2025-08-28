9f5a019284f6857997ec992792a59dd9e6cd5062
Dev093235 <dev431140@gmail.com>
2025-08-28 10:26:53 +0530

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,15 +2,15 @@ const Product = require('../models/product');
 
 exports.getAllProducts = (req, res) => {
   Product.getAll((err, results) => {
-    if (err) return res.status(500).json({ message: 'Server error' });
+    if (err) return res.status(500).send('Server error');
     res.json(results);
   });
 };
 
 exports.getProductById = (req, res) => {
   Product.getById(req.params.id, (err, results) => {
-    if (err) return res.status(500).json({ message: 'Server error' });
-    if (!results.length) return res.status(404).json({ message: 'Product not found' });
+    if (err) return res.status(500).send('Server error');
+    if (!results.length) return res.status(404).send('Product not found');
     res.json(results[0]);
   });
 };
@@ -18,28 +18,28 @@ exports.getProductById = (req, res) => {
 exports.createProduct = (req, res) => {
   const product = req.body;
   if (!product.name || !product.price)
-    return res.status(400).json({ message: 'Name and price required' });
+    return res.status(400).send('Name and price required');
 
-  Product.create(product, (err, results) => {
-    if (err) return res.status(500).json({ message: 'Server error' });
-    res.status(201).json({ message: 'Product created', id: results.insertId });
+  Product.create(product, (err, result) => {
+    if (err) return res.status(500).send('Server error');
+    res.status(201).json({ message: 'Product created', id: result.insertId });
   });
 };
 
 exports.updateProduct = (req, res) => {
   const product = req.body;
   if (!product.name || !product.price)
-    return res.status(400).json({ message: 'Name and price required' });
+    return res.status(400).send('Name and price required');
 
   Product.update(req.params.id, product, (err) => {
-    if (err) return res.status(500).json({ message: 'Server error' });
+    if (err) return res.status(500).send('Server error');
     res.json({ message: 'Product updated' });
   });
 };
 
 exports.deleteProduct = (req, res) => {
   Product.delete(req.params.id, (err) => {
-    if (err) return res.status(500).json({ message: 'Server error' });
+    if (err) return res.status(500).send('Server error');
     res.json({ message: 'Product deleted' });
   });
 };
