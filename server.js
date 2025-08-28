192d2ecb75c3758de47aa1e27b92ff514cb124c5
Dev093235 <dev431140@gmail.com>
2025-08-28 10:25:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,4 +10,4 @@ app.use(express.json());
 app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
